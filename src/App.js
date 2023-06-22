a495271a8d1918346467ee42c4da9aa8f958f05c
BKkom <seoy1108@gmail.com>
2023-06-22 18:01:54 +0900

    getMovies 함수
    async/await 사용. map을 이용해 배열 요소들을 보여줌. 자식 요소는 key를 가짐

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    // 셋업 함수 (해당 시스템에 연결하는 셋업 코드)
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []); // 컴포넌트의 모든 값을 포함한 의존성 목록
-  console.log(movies);
-  // 클린업 함수 반환 (해당 시스템과의 연결을 끊는 클린업 코드)
-  return <div>{loading ? <strong>Loading...</strong> : null}</div>;
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
